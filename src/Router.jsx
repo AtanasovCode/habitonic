3064d1dcbcee5cd2bd9f51e49a471a16b8532bbd
AtanasovCode <a_ljupce@yahoo.com>
2023-07-27 12:21:55 +0200

    initial file setup

---
+++ src/Router.jsx
@@ -0,0 +1,30 @@
+//importing from react-router
+import { 
+    createBrowserRouter,
+    RouterProvider,
+} from 'react-router-dom';
+
+//Importing routes
+import Homepage from './routes/Homepage';
+
+const Router = () => {
+
+    //creating the router with all the paths
+    const router = createBrowserRouter = ([
+        {
+            path: "/",
+            element: <Homepage />,
+            //errorElement: <Error />
+        }
+    ])
+
+    return (
+        <div className="global-container">
+            {/*Passing our created routes to the router provider*/}
+            <RouterProvider router={router} />
+        </div>
+    );
+}
+
+//Exporting Router component
+export default Router;
---
+++ src/routes/Homepage.jsx
@@ -0,0 +1,7 @@
+const Homepage = () => {
+    return (
+        <div className="home-container"></div>
+    );
+}
+
+export default Homepage;
